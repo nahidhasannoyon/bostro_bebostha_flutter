7c401467ab6717b095212f1d4ef3a123b905e8de
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2023-02-03 01:14:09 +0600

    adding routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:fardin_project/views/home/favorite_page.dart';
 import 'package:fardin_project/views/starter/first_screen.dart';
 import 'package:fardin_project/views/starter/second_screen.dart';
 import 'package:fardin_project/views/auth/sign_in_screen.dart';
 import 'package:fardin_project/views/auth/sign_up_screen.dart';
 import 'package:flutter/material.dart';
 
+import 'views/home/bottom_nav_bar_widget.dart';
+import 'views/home/home_page.dart';
+import 'views/home/recent_page.dart';
+import 'views/home/setting_page.dart';
 import 'views/provide_info_screen.dart';
 
 void main() {
@@ -25,6 +30,13 @@ class MyApp extends StatelessWidget {
         SignUpScreen.routeName: (context) => const SignUpScreen(),
         SignInScreen.routeName: (context) => const SignInScreen(),
         ProvideInfoScreen.routeName: (context) => const ProvideInfoScreen(),
+
+        // * homepage routes
+        BottomNavBarWidget.routeName: (context) => const BottomNavBarWidget(),
+        HomePage.routeName: (context) => const HomePage(),
+        FavoritePage.routeName: (context) => const FavoritePage(),
+        RecentPage.routeName: (context) => const RecentPage(),
+        SettingPage.routeName: (context) => const SettingPage(),
       },
     );
   }
