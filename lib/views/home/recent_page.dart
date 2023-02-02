dffa6f63dfad4869fa71e40c706c2b1d3042e6dc
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2023-02-03 01:13:10 +0600

    adding recent page

---
+++ lib/views/home/recent_page.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+import '../../controllers/style_controller.dart';
+import 'campaign_card_with_label.dart';
+
+class RecentPage extends StatelessWidget {
+  static const routeName = '/recent';
+  const RecentPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/home_page_bg.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // * Custom App Bar
+                CustomAppBar(size: size),
+
+                const SizedBox(
+                  height: 20,
+                ),
+                const CampaignCardWithLabel(
+                  label: 'Your Recent Activities',
+                ),
+                const CampaignCardWithLabel(
+                  label: 'Your Created Campaigns',
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color.fromRGBO(194, 231, 231, 1),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(20),
+          bottomRight: Radius.circular(20),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Fahim, Your',
+                      style: TextStyle(
+                        color: kSecondaryColor,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Favorite List',
+                      style: TextStyle(
+                        color: kSecondaryColor,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                Column(
+                  children: const [
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage:
+                          AssetImage('assets/images/profile_pic.png'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
